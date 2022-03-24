3375cf845f0e3171f4bc2d0c074cbf2fefc12cc9
5witchkr <hob4419@gmail.com>
2022-03-24 22:56:29 +0900

    auth(user)dto validatior

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    //todo 회원가입.
+    //validationPip를 만들어줄것임, type은 auth-Dto
+    //로직은 authservice-join으로 할것
+}
---
+++ src/auth/auth.dto.ts
@@ -0,0 +1,24 @@
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+
+export class AuthDto {
+    @IsString()
+    @MinLength(4)
+    @IsEmail()
+    @MaxLength(40)
+    email: string;
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    @Matches(/^[a-zA-Z0-9]*$/, {
+        message: 'password only accepts english and number'
+    })
+    password: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
+    @MaxLength(20)
+    nickname: string;
+}
