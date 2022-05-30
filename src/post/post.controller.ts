a76119f2677fb364fe9dfd4d83a46a6cbeddc43b
5witchkr <hob4419@gmail.com>
2022-05-30 23:46:46 +0900

    fix: post-repository-findTitle * post-repository-findTitle [title,nickname] -> [id,title,nickname.date]
    todo.. page, sort

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -12,6 +12,7 @@ export class PostController {
 
 
     //get simple page (no jwt)
+    //todo page sort
     @Get('')
     getPostTitle(): Promise<PostTitleDto[] | null> {
         return this.postService.getPostTitleService();
