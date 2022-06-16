4f4d1c0a6088e5d0d06ae32eab8809c7e6e103a1
5witchkr <hob4419@gmail.com>
2022-06-16 23:36:12 +0900

    feat: comment validate

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { PostRepository } from 'src/post/post.repository';
 import { Model } from 'mongoose';
 import { CommentDto } from './dto/comment.dto';
 import { Comments } from './comment.schema';
+import { AuthDto } from 'src/auth/dto/auth.dto';
 
 @Injectable()
 export class CommentService {
@@ -21,17 +22,20 @@ export class CommentService {
         }
     }
 
-    async createComment(id: string, commentDto: CommentDto) {
+    async createComment(id: string, commentDto: CommentDto, authDto: AuthDto) {
         try {
-            const targetPost = await this.postRepository.findPost(id)
-            const { nickname, commentBody } = commentDto;
-
-            const newComments = new this.commentsModel({
+            if (commentDto.nickname == authDto.nickname) {
+                const targetPost = await this.postRepository.findPost(id)
+                const { nickname, commentBody } = commentDto;
+                const newComments = new this.commentsModel({
                 nickname : nickname,
                 commentBody,
                 info: targetPost._id,
-            });
-            return await newComments.save();
+                });
+                return await newComments.save();
+            } else {
+                throw new UnauthorizedException('Fail user information validate')
+            }
         } catch (error) {
             throw new BadRequestException(error);
         }
